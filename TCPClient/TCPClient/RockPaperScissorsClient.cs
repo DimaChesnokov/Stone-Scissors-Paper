8ac313822ef411739463abef476044e109ab954b
DimaChesnokov <100202160+DimaChesnokov@users.noreply.github.com>
2023-12-03 16:52:24 +0300

    d

--- TCPClient/TCPClient/RockPaperScissorsClient.cs
+++ TCPClient/TCPClient/RockPaperScissorsClient.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace TCPClient
 {
-
     //Для игры "камень, ножницы, бумага" клиентская часть должна позволить игрокам вводить свой выбор
     //и отправлять его серверу для соперничества с другим игроком.
     //Вот пример кода для клиентской части игры:
@@ -22,34 +21,44 @@ namespace TCPClient
         {
             try
             {
+                // Установка порта и IP-адреса сервера
                 int port = 8888;
                 string serverIP = "127.0.0.1";
 
+                // Создание экземпляра TcpClient для соединения с сервером
                 TcpClient client = new TcpClient(serverIP, port);
 
+                // Получение потока для передачи и получения данных от сервера
                 NetworkStream stream = client.GetStream();
 
+                // Приветствие и запрос выбора игрового хода от пользователя
                 Console.WriteLine("Добро пожаловать в камень - ножницы - бумага!");
                 Console.WriteLine("Выбери чем будете ходить: камень, ножницы, бумага");
                 string choice = Console.ReadLine().ToLower();
 
+                // Преобразование выбора пользователя в массив байтов и отправка на сервер
                 byte[] data = Encoding.UTF8.GetBytes(choice);
                 stream.Write(data, 0, data.Length);
 
+                // Получение ответа (выбора оппонента) от сервера
                 data = new byte[256];
                 int bytes = stream.Read(data, 0, data.Length);
                 string opponentChoice = Encoding.UTF8.GetString(data, 0, bytes);
 
+                // Вывод выбора оппонента на экран
                 Console.WriteLine($"Opponent's choice: {opponentChoice}");
 
+                // Закрытие потока и клиента после завершения игры
                 stream.Close();
                 client.Close();
             }
             catch (Exception ex)
             {
+                // Обработка и вывод исключения, если оно возникло
                 Console.WriteLine($"Exception: {ex.Message}");
             }
 
+            // Уведомление о закрытии клиента
             Console.WriteLine("Client closed.");
         }
     }
--- TCPserver/TCPserver/Program.cs
+++ TCPserver/TCPserver/Program.cs
@@ -9,23 +9,24 @@ using System.Threading.Tasks;
 
 namespace ClientServer
 {
+    //Для того чтобы позволить двум пользователям играть друг с
+    //другом в камень, ножницы, бумагу через сервер, вам потребуется реализовать логику
+    //обработки выборов обоих игроков на сервере. Давайте создадим простой сервер для этой
+    //игры, который будет принимать соединения от двух клиентов и рассылать результаты их игры.
+
+
+    //Запустите сервер и затем две копии клиентской части, каждую в своем отдельном окне или процессе.
+    //Убедитесь, что клиенты подключаются к тому же IP-адресу и порту, на котором работает сервер.
+
+    //Пример сервера для игры в камень, ножницы, бумагу:
+
+
     internal class RockPaperScissorsServer
     {
         static TcpListener server;
         static TcpClient player1;
         static TcpClient player2;
 
-
-        //Для того чтобы позволить двум пользователям играть друг с
-        //другом в камень, ножницы, бумагу через сервер, вам потребуется реализовать логику
-        //обработки выборов обоих игроков на сервере. Давайте создадим простой сервер для этой
-        //игры, который будет принимать соединения от двух клиентов и рассылать результаты их игры.
-
-
-        //Запустите сервер и затем две копии клиентской части, каждую в своем отдельном окне или процессе.
-        //Убедитесь, что клиенты подключаются к тому же IP-адресу и порту, на котором работает сервер.
-
-        //Пример сервера для игры в камень, ножницы, бумагу:
         static void Main()
         {
             try
@@ -33,15 +34,19 @@ namespace ClientServer
                 int port = 8888;
                 server = new TcpListener(IPAddress.Any, port);
 
+                // Начало прослушивания подключений
                 server.Start();
                 Console.WriteLine("Ожидание игроков.");
 
+                // Ожидание и подключение первого игрока
                 player1 = server.AcceptTcpClient();
                 Console.WriteLine("Игрок 1 подключён.");
 
+                // Ожидание и подключение второго игрока
                 player2 = server.AcceptTcpClient();
                 Console.WriteLine("Игрок 2 подключён.");
 
+                // Создание потоков для обработки каждого игрока параллельно
                 Thread thread1 = new Thread(HandlePlayer);
                 thread1.Start(player1);
 
@@ -59,6 +64,7 @@ namespace ClientServer
             TcpClient client = (TcpClient)obj;
             NetworkStream stream = client.GetStream();
 
+            // Определение текущего игрока по его подключению
             string player = (client == player1) ? "Игрок 1" : "Игрок 2";
 
             try
@@ -68,15 +74,18 @@ namespace ClientServer
                     byte[] data = new byte[256];
                     int bytes = stream.Read(data, 0, data.Length);
 
+                    // Проверка на отключение игрока
                     if (bytes == 0)
                     {
                         Console.WriteLine($"{player} отключён.");
                         break;
                     }
 
+                    // Получение выбора игрока и вывод на экран
                     string choice = Encoding.UTF8.GetString(data, 0, bytes);
                     Console.WriteLine($"{player} выбор: {choice}");
 
+                    // Отправка выбора другому игроку (если он подключен)
                     if (client == player1)
                     {
                         if (player2 != null)
@@ -97,6 +106,7 @@ namespace ClientServer
             {
                 Console.WriteLine($"Ошибка: {ex.Message}");
             }
+            Console.ReadLine();
         }
 
         static void SendChoice(TcpClient recipient, string choice)
@@ -107,4 +117,3 @@ namespace ClientServer
         }
     }
 }
-
