c23f883e5ccaea50875ca9fe3af0fe720fe4f9a0
DimaChesnokov <100202160+DimaChesnokov@users.noreply.github.com>
2023-12-04 16:07:54 +0300

    hfgh

--- TCPClient/TCPClient/RockPaperScissorsClient.cs
+++ TCPClient/TCPClient/RockPaperScissorsClient.cs
@@ -1,64 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace TCPClient
 {
-    //Для игры "камень, ножницы, бумага" клиентская часть должна позволить игрокам вводить свой выбор
-    //и отправлять его серверу для соперничества с другим игроком.
-    //Вот пример кода для клиентской части игры:
-
-    //Этот код представляет простую клиентскую часть игры.
-    //Он подключается к серверу, позволяет игроку ввести свой выбор (камень, ножницы или бумага) и отправляет его серверу.
-    //Затем клиент ожидает ответа от сервера с выбором оппонента и выводит его на экран.
-
     internal class RockPaperScissorsClient
     {
         static void Main(string[] args)
         {
             try
             {
-                // Установка порта и IP-адреса сервера
                 int port = 8888;
                 string serverIP = "127.0.0.1";
-
-                // Создание экземпляра TcpClient для соединения с сервером
                 TcpClient client = new TcpClient(serverIP, port);
-
-                // Получение потока для передачи и получения данных от сервера
                 NetworkStream stream = client.GetStream();
 
-                // Приветствие и запрос выбора игрового хода от пользователя
+                byte[] data = new byte[256];
+                int bytes = stream.Read(data, 0, data.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(data, 0, bytes));
+
+                string playerName = "";
+                if (bytes > 0)
+                {
+                    Console.WriteLine("Введите ваше имя:");
+                    playerName = Console.ReadLine();
+                    data = Encoding.UTF8.GetBytes(playerName);
+                    stream.Write(data, 0, data.Length);
+                }
+
                 Console.WriteLine("Добро пожаловать в камень - ножницы - бумага!");
                 Console.WriteLine("Выбери чем будете ходить: камень, ножницы, бумага");
                 string choice = Console.ReadLine().ToLower();
 
-                // Преобразование выбора пользователя в массив байтов и отправка на сервер
-                byte[] data = Encoding.UTF8.GetBytes(choice);
+                data = Encoding.UTF8.GetBytes(choice);
                 stream.Write(data, 0, data.Length);
 
-                // Получение ответа (выбора оппонента) от сервера
                 data = new byte[256];
-                int bytes = stream.Read(data, 0, data.Length);
-                string opponentChoice = Encoding.UTF8.GetString(data, 0, bytes);
+                bytes = stream.Read(data, 0, data.Length);
+                Console.WriteLine(Encoding.UTF8.GetString(data, 0, bytes));
 
-                // Вывод выбора оппонента на экран
-                Console.WriteLine($"Opponent's choice: {opponentChoice}");
-
-                // Закрытие потока и клиента после завершения игры
                 stream.Close();
                 client.Close();
             }
             catch (Exception ex)
             {
-                // Обработка и вывод исключения, если оно возникло
                 Console.WriteLine($"Exception: {ex.Message}");
             }
 
-            // Уведомление о закрытии клиента
             Console.WriteLine("Client closed.");
         }
     }
--- TCPserver/TCPserver/Program.cs
+++ TCPserver/TCPserver/Program.cs
@@ -1,26 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Sockets;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ClientServer
 {
-    //Для того чтобы позволить двум пользователям играть друг с
-    //другом в камень, ножницы, бумагу через сервер, вам потребуется реализовать логику
-    //обработки выборов обоих игроков на сервере. Давайте создадим простой сервер для этой
-    //игры, который будет принимать соединения от двух клиентов и рассылать результаты их игры.
-
-
-    //Запустите сервер и затем две копии клиентской части, каждую в своем отдельном окне или процессе.
-    //Убедитесь, что клиенты подключаются к тому же IP-адресу и порту, на котором работает сервер.
-
-    //Пример сервера для игры в камень, ножницы, бумагу:
-
-
     internal class RockPaperScissorsServer
     {
         static TcpListener server;
@@ -33,25 +18,22 @@ namespace ClientServer
             {
                 int port = 8888;
                 server = new TcpListener(IPAddress.Any, port);
-
-                // Начало прослушивания подключений
                 server.Start();
                 Console.WriteLine("Ожидание игроков.");
 
-                // Ожидание и подключение первого игрока
                 player1 = server.AcceptTcpClient();
                 Console.WriteLine("Игрок 1 подключён.");
+                SendNameRequest(player1);
 
-                // Ожидание и подключение второго игрока
                 player2 = server.AcceptTcpClient();
                 Console.WriteLine("Игрок 2 подключён.");
+                SendNameRequest(player2);
 
-                // Создание потоков для обработки каждого игрока параллельно
-                Thread thread1 = new Thread(HandlePlayer);
-                thread1.Start(player1);
+                Thread thread1 = new Thread(() => HandlePlayer(player1, player2));
+                thread1.Start();
 
-                Thread thread2 = new Thread(HandlePlayer);
-                thread2.Start(player2);
+                Thread thread2 = new Thread(() => HandlePlayer(player2, player1));
+                thread2.Start();
             }
             catch (Exception ex)
             {
@@ -59,13 +41,10 @@ namespace ClientServer
             }
         }
 
-        static void HandlePlayer(object obj)
+        static void HandlePlayer(TcpClient currentPlayer, TcpClient opponent)
         {
-            TcpClient client = (TcpClient)obj;
-            NetworkStream stream = client.GetStream();
-
-            // Определение текущего игрока по его подключению
-            string player = (client == player1) ? "Игрок 1" : "Игрок 2";
+            NetworkStream stream = currentPlayer.GetStream();
+            string playerName = GetPlayerName(currentPlayer);
 
             try
             {
@@ -74,45 +53,44 @@ namespace ClientServer
                     byte[] data = new byte[256];
                     int bytes = stream.Read(data, 0, data.Length);
 
-                    // Проверка на отключение игрока
                     if (bytes == 0)
                     {
-                        Console.WriteLine($"{player} отключён.");
+                        Console.WriteLine($"{playerName} отключён.");
                         break;
                     }
 
-                    // Получение выбора игрока и вывод на экран
                     string choice = Encoding.UTF8.GetString(data, 0, bytes);
-                    Console.WriteLine($"{player} выбор: {choice}");
+                    Console.WriteLine($"{playerName} выбор: {choice}");
 
-                    // Отправка выбора другому игроку (если он подключен)
-                    if (client == player1)
-                    {
-                        if (player2 != null)
-                        {
-                            SendChoice(player2, choice);
-                        }
-                    }
-                    else if (client == player2)
-                    {
-                        if (player1 != null)
-                        {
-                            SendChoice(player1, choice);
-                        }
-                    }
+                    SendChoice(opponent, playerName, choice);
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Ошибка: {ex.Message}");
             }
-            Console.ReadLine();
         }
 
-        static void SendChoice(TcpClient recipient, string choice)
+
+        static void SendNameRequest(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] data = Encoding.UTF8.GetBytes("Введите ваше имя:");
+            stream.Write(data, 0, data.Length);
+        }
+
+        static string GetPlayerName(TcpClient client)
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] data = new byte[256];
+            int bytes = stream.Read(data, 0, data.Length);
+            return Encoding.UTF8.GetString(data, 0, bytes);
+        }
+
+        static void SendChoice(TcpClient recipient, string playerName, string choice)
         {
             NetworkStream stream = recipient.GetStream();
-            byte[] data = Encoding.UTF8.GetBytes(choice);
+            byte[] data = Encoding.UTF8.GetBytes($"{playerName}: {choice}");
             stream.Write(data, 0, data.Length);
         }
     }
