7588732db0dca90443de1877af75306b7a9f6d95
DimaChesnokov <100202160+DimaChesnokov@users.noreply.github.com>
2023-12-05 12:58:38 +0300

    камень-ножницы-бумага

---
+++ WindowsFormsRockScissorsPapier/WindowsFormsRockScissorsPapier/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace WindowsFormsRockScissorsPapier
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radio_server = new System.Windows.Forms.RadioButton();
+            this.radio_client = new System.Windows.Forms.RadioButton();
+            this.text_ip = new System.Windows.Forms.TextBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_result = new System.Windows.Forms.Label();
+            this.text_debug = new System.Windows.Forms.TextBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.button_clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radio_server
+            // 
+            this.radio_server.AutoSize = true;
+            this.radio_server.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radio_server.Location = new System.Drawing.Point(29, 33);
+            this.radio_server.Name = "radio_server";
+            this.radio_server.Size = new System.Drawing.Size(106, 31);
+            this.radio_server.TabIndex = 0;
+            this.radio_server.TabStop = true;
+            this.radio_server.Text = "Сервер";
+            this.radio_server.UseVisualStyleBackColor = true;
+            this.radio_server.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radio_client
+            // 
+            this.radio_client.AutoSize = true;
+            this.radio_client.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radio_client.Location = new System.Drawing.Point(29, 70);
+            this.radio_client.Name = "radio_client";
+            this.radio_client.Size = new System.Drawing.Size(108, 31);
+            this.radio_client.TabIndex = 1;
+            this.radio_client.TabStop = true;
+            this.radio_client.Text = "Клиент";
+            this.radio_client.UseVisualStyleBackColor = true;
+            // 
+            // text_ip
+            // 
+            this.text_ip.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.text_ip.Location = new System.Drawing.Point(29, 114);
+            this.text_ip.Name = "text_ip";
+            this.text_ip.Size = new System.Drawing.Size(108, 34);
+            this.text_ip.TabIndex = 2;
+            this.text_ip.Text = "127.0.0.1";
+            this.text_ip.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button_start
+            // 
+            this.button_start.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_start.Location = new System.Drawing.Point(151, 114);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(133, 36);
+            this.button_start.TabIndex = 3;
+            this.button_start.Text = "Старт";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(290, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(133, 36);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Бумага";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 204);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 36);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Камень";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(151, 204);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 36);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Ножницы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_result
+            // 
+            this.label_result.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label_result.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label_result.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_result.Location = new System.Drawing.Point(12, 254);
+            this.label_result.Name = "label_result";
+            this.label_result.Size = new System.Drawing.Size(411, 63);
+            this.label_result.TabIndex = 7;
+            this.label_result.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // text_debug
+            // 
+            this.text_debug.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.text_debug.Location = new System.Drawing.Point(466, 33);
+            this.text_debug.Multiline = true;
+            this.text_debug.Name = "text_debug";
+            this.text_debug.ReadOnly = true;
+            this.text_debug.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.text_debug.Size = new System.Drawing.Size(309, 265);
+            this.text_debug.TabIndex = 8;
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Enabled = false;
+            this.button_clear.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_clear.Location = new System.Drawing.Point(642, 304);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(133, 36);
+            this.button_clear.TabIndex = 9;
+            this.button_clear.Text = "Очистить";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.text_debug);
+            this.Controls.Add(this.label_result);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.text_ip);
+            this.Controls.Add(this.radio_client);
+            this.Controls.Add(this.radio_server);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Камень-Ножницы-Бумага";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radio_server;
+        private System.Windows.Forms.RadioButton radio_client;
+        private System.Windows.Forms.TextBox text_ip;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_result;
+        private System.Windows.Forms.TextBox text_debug;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Button button_clear;
+    }
+}
+
---
+++ WindowsFormsRockScissorsPapier/WindowsFormsRockScissorsPapier/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace WindowsFormsRockScissorsPapier
+{
+    public partial class Form1 : Form
+    {
+        int port = 12345;
+        StreamReader reader; //чтение
+        StreamWriter writer; //запись
+        int count = 0;
+        bool sent_hand = false;
+        bool read_hand = false;
+        string my_hand = "";
+        string op_hand = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setButtons(bool enable)
+        {
+            button1.Enabled = enable;
+            button2.Enabled = enable;
+            button3.Enabled = enable;
+            button_clear.Enabled = enable;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            //try
+            //{
+            if (radio_server.Checked)
+                StartServer();
+
+            if (radio_client.Checked)
+                StartClient();
+
+            setButtons(true);
+            timer.Enabled = true;
+            text_debug.Text = text_debug.Text + "Соеднинение установлено" + Environment.NewLine;
+            button_start.Enabled = false;
+            //}
+            //{
+            //    MessageBox.Show("Убедитесь, что выбрали сервер и клиент");
+            //}
+
+        }
+
+        private void StartServer()
+        {
+            //try
+            //{
+            //инициализация на каком ip адресе работать
+            TcpListener listener = new TcpListener(new IPEndPoint(IPAddress.Parse(text_ip.Text), port));
+            listener.Start(); //запуск
+            TcpClient server = listener.AcceptTcpClient(); //создание tcp клиента для сервера. (принимаем) 
+            server.ReceiveTimeout = 500;
+            //потоки для чтения или записи
+            reader = new StreamReader(server.GetStream()); //получаем поток для чтения
+            writer = new StreamWriter(server.GetStream()); // для записи
+            writer.AutoFlush = true;
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Убедитесь, что выбрали сервер - клиент");
+            //}
+        }
+
+        private void StartClient()
+        {
+
+
+
+            TcpClient client = new TcpClient();
+            client.Connect(text_ip.Text, port);
+            client.ReceiveTimeout = 500;
+            reader = new StreamReader(client.GetStream());
+            writer = new StreamWriter(client.GetStream());
+
+            writer.AutoFlush = true;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            send("Камень");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            send("Ножницы");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            send("Бумага");
+        }
+
+        private void send(string text)
+        {
+            //try
+            //{
+            if (sent_hand)
+                return;
+            writer.WriteLine(text);
+            // text_debug.Text = text_debug.Text + "Отправлено: " + text + Environment.NewLine;
+            sent_hand = true;
+            my_hand = text;
+            setButtons(false);
+            //text_debug.Text = text_debug.Text + "Отправлено: " + text + Environment.NewLine;
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Убедитесь, что выбрали сервер - клиент");
+            //}
+
+
+        }
+
+        private string read()
+        {
+
+            if (read_hand)
+                return "";
+            try
+            {
+                string text;
+                text = reader.ReadLine();
+                // text_debug.Text = text_debug.Text +ы "Прочитано: " + text + Environment.NewLine;
+                read_hand = true;
+                op_hand = text;
+
+
+
+                return text;
+            }
+            catch
+            {
+                return "";
+            }
+
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+
+            //string  s = ()
+            string hand = read();
+            if (my_hand != "" & op_hand != "")
+            {
+                count = count + 1;
+                text_debug.Text = text_debug.Text + "Партия " + count + Environment.NewLine;
+                text_debug.Text = text_debug.Text + "Отправлено: " + my_hand + Environment.NewLine;
+                text_debug.Text = text_debug.Text + "Прочитано: " + op_hand + Environment.NewLine;
+
+            }
+
+
+            if (sent_hand && read_hand)
+            {
+
+                ResultGame();
+                text_debug.Text = text_debug.Text + " " + Environment.NewLine;
+            }
+
+
+
+
+
+
+        }
+
+        private void ResultGame()
+        {
+            string res = CompareHand(my_hand, op_hand);
+            text_debug.Text = text_debug.Text + "Мой ход: " + my_hand + Environment.NewLine;
+            text_debug.Text = text_debug.Text + "Ход противника: " + op_hand + Environment.NewLine;
+            label_result.Text = my_hand + " vs  " + op_hand + Environment.NewLine;
+            label_result.Text = label_result.Text + res;
+            sent_hand = false;
+            read_hand = false;
+            setButtons(true);
+            my_hand = "";
+            op_hand = "";
+            System.Threading.Thread.Sleep(1000);
+        }
+
+        private string CompareHand(string hand1, string hand2)
+        {
+            if (hand1 == hand2)
+                return "Ничья"; //ничья
+
+            if (hand1 == "Камень")
+                if (hand2 == "Ножницы")
+                    return "Победа"; //первый
+                else
+                    return "Проигрыш"; // второй игрок
+
+            if (hand1 == "Ножницы")
+                if (hand2 == "Бумага")
+                    return "Победа"; //первый
+                else
+                    return "Проигрыш"; // второй игрок
+
+            if (hand1 == "Бумага")
+                if (hand2 == "Камень")
+                    return "Победа"; //первый
+                else
+                    return "Проигрыш"; // второй игрок
+            return "Ничья";
+
+        }
+
+
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+
+            text_debug.Text = "";
+            count = 0;
+            sent_hand = false;
+            read_hand = false;
+            my_hand = "";
+            op_hand = "";
+            label_result.Text = "";
+
+        }
+    }
+}
